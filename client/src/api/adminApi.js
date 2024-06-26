44b5b58e972b1563280c50d8b23ffb73285fee11
harsh <harsh@gmail.com>
2024-06-26 11:27:42 +0530

    api call

--- client/src/api/adminApi.js
+++ client/src/api/adminApi.js
@@ -9,3 +9,11 @@ export async function postLoginAdmin(email,password){
     // configData.params = {email,password}
     return {apiEndPoint, configData}
 }
+
+export async function getUserList(){
+    let baseUrlAdmin = profileParser() + "api/admin"
+    let apiEndPoint = baseUrlAdmin + "/fetchAllUser"
+    let configData = await configurateAsync(false) //isBearerAuth=false
+    // configData.params = {email,password}
+    return {apiEndPoint, configData}
+}
--- client/src/components/Admin/Content/UserManagement/ManageUser/ManageUser.js
+++ client/src/components/Admin/Content/UserManagement/ManageUser/ManageUser.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getAPICall } from '../../../../../api/common/rxApiStoreBase';
+import { getUserList } from '../../../../../api/adminApi';
 
 export default function ManageUser() {
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+
+  const fetchUserData = async() => {
+    const apiRequestData =await getUserList();
+    const postLoginApi$ = await getAPICall(apiRequestData.apiEndPoint,apiRequestData.configData)
+
+    postLoginApi$.subscribe({
+      next:(res)=>{
+        console.log("re::::::",res)
+      },
+      error:(err)=>{
+        console.log("err:",err);
+      }
+    })
+  }
+  
   return (
     <div>manageUser</div>
   )
